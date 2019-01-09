09a31121f940b2c9e419bb5c49b74e8c40f9d0b3
websterkk <Webster.kj@gmail.com>
2019-01-08 20:20:55 -0500

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,175 @@
+// JAVASCRIPT FUNCTIONS FOR MAIN PAGE OF WEBSITE
+
+var tabIndex;
+var lastIndex = 0;
+var linkIndex;
+
+function showResume() {
+    tabIndex = 1;
+    hideAll();
+    lastIndex = 1;
+}
+
+function openMenu() {
+    document.getElementById("ddMenu").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches("#menuBtn")) {
+        if (document.getElementById("ddMenu").classList.contains('show')) {
+            document.getElementById("ddMenu").classList.remove('show');
+        }
+    }
+}
+
+function openModal(curPage, row) {
+    var thisMod;
+    if (curPage == 1)
+        thisMod = modal.school;
+    else thisMod = modal.personal;
+    var modImg = thisMod[row][0];
+    var modAbs = thisMod[row][1];
+    linkIndex = row;
+    document.getElementById("modalImage").style.backgroundImage = modImg;
+    document.getElementById("modalAbstract").innerHTML = modAbs;
+    document.getElementById("modalBackground").style.display = "block";
+    //linkIndex = modal.personal[row][col];
+}
+
+function goToPage(linkIndex) {
+    window.open(linkIndex);
+    linkIndex = '';
+}
+
+function hideAll() {
+    document.getElementById("resumeContainer").display = none;
+    document.getElementById("htmlContainer").display = none;
+    document.getElementById("schoolContainer").display = none;
+    document.getElementById("externalContainer").display = none;
+    if (tabIndex == lastIndex) {
+        document.getElementById("smallTitle").display = none;
+        document.getElementById("bigTitle").display = block;
+    } else {
+        document.getElementById("bigTitle").display = none;
+        document.getElementById("smallTitle").display = block;
+        if (tabIndex === 1) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 2) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 3) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 4) {
+            document.getElementById("resumeContainer").display = block;
+        }
+    }
+}
+
+function playVideo() {
+    var vidSource = a;
+    var vidTitle = b;
+    var pgSrc = '<!DOCTYPE html>' +
+        '<html lang="en">' +
+        '<head>' +
+        '<meta charset="UTF-8">' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+        '<meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+        '<link rel="stylesheet" href="home.css">' +
+        '<title>' + vidTitle + '</title>' +
+        '</head>' +
+        '<body>' +
+        '<video width="1000" height="600" controls class="videoPlayer">' +
+        '<source src=' + vidSource + ' type="video/mp4">' +
+        '</video>' +
+        '</body>' +
+        '</html>';
+    // Create Page from pgSrc
+}
+
+/*
+
+function goToPage() {
+    var linkType = thisMod[row][2];
+    if (linkType == 0){
+        // play video
+        alert (linkIndex + " yt")
+        playVideo(thisMod[row][3])
+    } else if (linkType == 1) {
+        // open external page
+    } else {
+        // open local page
+    }
+    //window.open(linkIndex);
+    linkIndex = '';
+    thisMod = '';
+}
+
+function hideAll() {
+    document.getElementById("resumeContainer").display = none;
+    document.getElementById("htmlContainer").display = none;
+    document.getElementById("schoolContainer").display = none;
+    document.getElementById("externalContainer").display = none;
+    if (tabIndex == lastIndex) {
+        document.getElementById("smallTitle").display = none;
+        document.getElementById("bigTitle").display = block;
+    } else {
+        document.getElementById("bigTitle").display = none;
+        document.getElementById("smallTitle").display = block;
+        if (tabIndex === 1) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 2) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 3) {
+            document.getElementById("resumeContainer").display = block;
+        }
+        if (tabIndex === 4) {
+            document.getElementById("resumeContainer").display = block;
+        }
+    }
+}
+
+function playVideo() {
+    var vidSource = a;
+    var vidTitle = b;
+    var pgSrc = '<!DOCTYPE html>' +
+        '<html lang="en">' +
+        '<head>' +
+        '<meta charset="UTF-8">' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+        '<meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+        '<link rel="stylesheet" href="home.css">' +
+        '<title>' + vidTitle + '</title>' +
+        '</head>' +
+        '<body>' +
+        '<video width="1000" height="600" controls class="videoPlayer">' +
+        '<source src=' + vidSource + ' type="video/mp4">' +
+        '</video>' +
+        '</body>' +
+        '</html>';
+    // Create Page from pgSrc
+}
+
+function ytVideo(thisVideo) {
+    var pgSrc = '<!DOCTYPE html>' +
+    '<html lang="en">' +
+    '<head>' +
+    '<meta charset="UTF-8">' +
+    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+    '<meta http-equiv="X-UA-Compatible" content="ie=edge">' +
+    '<link rel="stylesheet" href="./css/home.css">' +
+    '<title>Video</title>' +
+    '</head>' +
+    '<body>' +
+    '<iframe width="1000" height="600" class="videoPlayer"' +
+    'src="https://www.youtube.com/embed/8rnvHHzSTzE?rel=0&amp;showinfo=0" ' +
+    'frameborder="0" allow="autoplay; encrypted-media"' +
+    'allowfullscreen>' +
+    '</iframe>' +
+    '</body>' +
+    '</html>' +
+}
+*/
---
+++ js/modaljson.js
@@ -0,0 +1,24 @@
+var modal = {
+    "personal": [
+        ['url("./img/facebook.png")','Thesis Stuff',1,'https://scholarsarchive.byu.edu/etd/6094/'],
+        ['url("./img/photoalbm.png")','Photo Album Website',1,'http://websterkk.github.io'],
+        ['url("./img/Zombie.jpg")','Oak bookcase',1],
+        ['url("./img/Zombie.jpg")','Bed frame',1],
+        ['url("./img/Zombie.jpg")','Coffee table',1],
+        ['url("./img/Zombie.jpg")','Cedar Chest',1],
+        ['url("./img/Zombie.jpg")','Jewelry Box',1],
+        ['url("./img/Zombie.jpg")','Cherry Bookcase',1,'Planned as a bookcase, but ended up being used as a stereo cabinet. Made from Cherry with Birch panels'],
+        ['url("./img/scary.png")','Horror Video 2012',0,'https://www.youtube.com/embed/8rnvHHzSTzE?rel=0&amp;showinfo=0'],
+        ['url("./img/snowbike.png")','Mountain bike ride the morning of the first snowfall of Fall 2012. Filmed in South Fork Canyon with two of my friends. Music is Duluth by Trampled By Turtles',0],
+        ['url("./img/lucy.png")','Dedicated to Lucy, the best pet black widow my roommates and I ever had for three weeks',0],
+        ['url("./img/whiskey.png")','Cover of Whiskey by Trampled by Turtles',0]
+    ],
+    "school": [
+        ['url("./img/Zombie.jpg")','Photo Album Website',1,'http://websterkk.github.io'],
+        ['url("./img/star.png")','Presented at 2014 STAR Global Conference in Vienna, Austria',1,'http://mdx2.plm.automation.siemens.com/conference/star-global-conference-2014'],
+        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1, 'http://websterkk.github.io/me471'],
+        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1],
+        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1],
+        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1]
+    ]
+};
