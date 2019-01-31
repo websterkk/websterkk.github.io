8d892edb9d5f0ddb540701a80feab7bbb8f55da3
websterkk <kjw@tuta.io>
2019-01-30 22:43:46 -0500

    fixing images and links on school page

--- js/modaljson.js
+++ js/modaljson.js
@@ -1,24 +1,24 @@
 var modal = {
     "personal": [
-        ['url("../img/facebook.png")','Thesis Stuff','coming soon','https://scholarsarchive.byu.edu/etd/6094/'],
-        ['url("../img/photoalbm.png")','Photo Album Website','Visit Site','http://websterkk.github.io'],
-        ['url("../img/Zombie.jpg")','Oak bookcase','Coming Soon'],
-        ['url("../img/Zombie.jpg")','Bed frame','Coming Soon'],
-        ['url("../img/Zombie.jpg")','Coffee table','Coming Soon'],
-        ['url("../img/Zombie.jpg")','Cedar Chest','Coming Soon'],
-        ['url("../img/Zombie.jpg")','Jewelry Box','Coming Soon'],
-        ['url("../img/Zombie.jpg")','Cherry Bookcase','Coming Soon','Planned as a bookcase, but ended up being used as a stereo cabinet. Made from Cherry with Birch panels'],
+        ['url("../img/facebook.png")','JavaScript version of the board game "Machi Koro"','coming soon','https://scholarsarchive.byu.edu/etd/6094/'],
+        ['url("../img/photoalbm.png")','Photo Album Website','Visit Site','http://websterkk.github.io/internalpages/photoalbum.html'],
+        ['url("../img/comingSoon.jpg")','Oak bookcase','Coming Soon'],
+        ['url("../img/comingSoon.jpg")','Bed frame','Coming Soon'],
+        ['url("../img/comingSoon.jpg")','Coffee table','Coming Soon'],
+        ['url("../img/comingSoon.jpg")','Cedar Chest','Coming Soon'],
+        ['url("../img/comingSoon.jpg")','Jewelry Box','Coming Soon'],
+        ['url("../img/comingSoon.jpg")','Cherry Bookcase','Coming Soon','Planned as a bookcase, but ended up being used as a stereo cabinet. Made from Cherry with Birch panels'],
         ['url("../img/scary.png")','Horror Video 2012','Watch Video','https://websterkk.github.io/internalpages/video.html?y=8rnvHHzSTzE'],
         ['url("../img/snowbike.png")','Mountain bike ride the morning of the first snowfall of Fall 2012. Filmed in South Fork Canyon with two of my friends. Music is Duluth by Trampled By Turtles','Watch Video','https://websterkk.github.io/internalpages/video.html?y=PgdXazzkQJc'],
         ['url("../img/lucy.png")','Dedicated to Lucy, the best pet black widow my roommates and I ever had for three weeks','Watch Video','https://websterkk.github.io/internalpages/video.html?y=Cv_Y35jnTwA'],
         ['url("../img/whiskey.png")','Cover of Whiskey by Trampled by Turtles','Watch Video','https://websterkk.github.io/internalpages/video.html?y=7uKOnInJXhY']
     ],
     "school": [
-        ['url("./img/Zombie.jpg")','Photo Album Website',1,'http://websterkk.github.io'],
-        ['url("./img/star.png")','Presented at 2014 STAR Global Conference in Vienna, Austria',1,'http://mdx2.plm.automation.siemens.com/conference/star-global-conference-2014'],
-        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1, 'http://websterkk.github.io/me471'],
-        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1],
-        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1],
-        ['url("./img/scary.png")','Modeled and optimized the design of an RC Car as part of a group competition for \"Mechanical Design\" class',1]
+        ['url("./img/comingSoon.jpg")','Photo Album Website','Read Thesis','https://scholarsarchive.byu.edu/etd/6094/'],
+        ['url("./img/star.png")','Presented at 2014 STAR Global Conference in Vienna, Austria','View Presentation','http://mdx2.plm.automation.siemens.com/conference/star-global-conference-2014'],
+        ['url("./img/comingSoon.png")','Moon lander game created in C++ with Qt','coming soon', 'http://websterkk.github.io/me471'],
+        ['url("./img/comingSoon.png")','Integrated Leap Motion controller with 3D model in NX as part of a group project','coming soon'],
+        ['url("./img/comingSoon.png")','Video created with hand drawn images describing the formation of Zion Canyon','coming soon'],
+        ['url("./img/me471.png")','Modeled and optimized the design of an RC Car as part of a group competition','coming soon']
     ]
 };
